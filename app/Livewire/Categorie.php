8c2e61a6075091bad1d84f93d4b5f75188058c0f
MBA NDONG Chancel <mbandongjohan@gmail.com>
2023-09-03 17:30:38 +0100

    Ajout de CategorieComponent

--- app/Livewire/Categorie.php
+++ app/Livewire/Categorie.php
@@ -2,12 +2,57 @@
 
 namespace App\Livewire;
 
-use Livewire\Component;
+use App\Models\Categorie as ModelsCategorie;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Rule;
+use Livewire\Attributes\Title;
 
-class Categorie extends Component
+class Categorie extends AppComponent
 {
+    #[Rule('required|min:2|unique:categories')]
+    public $libelle = null;
+
+    public function save()
+    {
+        $this->validate();
+        $item = (!$this->edit_id) ? new ModelsCategorie() : ModelsCategorie::findOrFail($this->edit_id);
+        $item->libelle = $this->libelle;
+        $item->save();
+        $this->resetValues();
+        session()->flash('status', 'Saved successfully');
+    }
+
+    public function editItem(ModelsCategorie $item)
+    {
+        $this->edit_id = $item->id;
+        $this->libelle = $item->libelle;
+        $this->textSubmit = 'Modifier';
+    }
+
+    public function deleteItem(mixed $id)
+    {
+        parent::deleteItem(ModelsCategorie::findOrFail($id));
+    }
+
+    public function deleteConfirmed(mixed $id)
+    {
+        parent::deleteConfirmed(ModelsCategorie::findOrFail($id));
+        session()->flash('status', 'Deleted successfully');
+    }
+
+    public function resetValues()
+    {
+        parent::resetValues();
+        $this->libelle = null;
+        $this->dispatch('close-modal'); 
+    }
+
+    #[Layout('livewire.layouts.base')]
+    #[Title('Boutique | Categorie')]
     public function render()
     {
-        return view('livewire.categorie');
+        return view('livewire.categorie', [
+            'categories' => ModelsCategorie::all(),
+        ]);
     }
 }
--- resources/views/livewire/categorie.blade.php
+++ resources/views/livewire/categorie.blade.php
@@ -1,3 +1,61 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <div>
+        <form wire:submit="save">
+            <div>
+                <label for="libelle">Libellé</label>
+                <input wire:model.live="libelle" id="libelle" type="text">
+                @error('libelle') <span>{{ $message }}</span> @enderror
+            </div>
+
+            <div>
+                <input type="submit" value="{{ $this->textSubmit }}">
+                <input wire:click='resetValues' type="reset" value="Annuler">
+            </div>
+        </form>
+    </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Catégorie</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($categories as $item)
+                <tr
+                    wire:click='editItem({{ $item->id }})'
+                    wire:dblclick='deleteItem({{ $item->id }})'
+                >
+                    <td>{{ $item->libelle }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <!-- deleteModal -->
+    <div wire:ignore.self class="modal fade" id="confirmModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="viewModalLabel">Confirmer la suppression</h5>
+                    <button class="btn-close" wire:click="deleteCancelled()" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Suppression de &lt; {{ $this->libelle }} &gt;</p>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary btn-sm" wire:click="deleteConfirmed({{ $this->delete_id }})">
+                        Valider
+                    </button>
+                    <button class="btn btn-primary btn-sm" wire:click="deleteCancelled" data-bs-dismiss="modal">
+                        Annuler
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    @if (session()->has('status'))
+        <div class="alert alert-success text-center">{{ session('status') }}</div>
+    @endif
 </div>
