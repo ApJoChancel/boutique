8426fd58b721a7464366eade255b23e4d8081e2c
MBA NDONG Chancel <mbandongjohan@gmail.com>
2023-09-03 10:59:25 +0100

    Ajout de UserComponent

--- app/Livewire/User.php
+++ app/Livewire/User.php
@@ -5,11 +5,14 @@ namespace App\Livewire;
 use App\Models\Role;
 use App\Models\User as ModelsUser;
 use Illuminate\Support\Facades\Hash;
+use Livewire\Attributes\Layout;
 use Livewire\Attributes\Rule;
-use Livewire\Component;
+use Livewire\Attributes\Title;
 
-class User extends Component
+class User extends AppComponent
 {
+    const DEFAULT_PASSWORD = 'password';
+
     #[Rule('required|min:5|unique:users')]
     public $login = null;
     #[Rule('required')]
@@ -18,19 +21,56 @@ class User extends Component
     public function register()
     {
         $this->validate();
-        $item = new ModelsUser();
+        $item = (!$this->edit_id) ? new ModelsUser() : ModelsUser::findOrFail($this->edit_id);
         $item->login = $this->login;
-        $item->role_id = 1;
-        $item->password = Hash::make('password');
+        $item->role_id = $this->role_id;
+        $item->password = Hash::make(self::DEFAULT_PASSWORD);
         $item->save();
-        $this->login = $this->role = null;
+        $this->resetValues();
+        session()->flash('status', 'Added successfully');
     }
 
+    public function editItem(ModelsUser $user)
+    {
+        $this->edit_id = $user->id;
+        $this->login = $user->login;
+        $this->role_id = $user->role_id;
+        $this->textSubmit = 'Modifier';
+    }
+
+    public function deleteItem(mixed $id)
+    {
+        parent::deleteItem(ModelsUser::findOrFail($id));
+    }
+
+    public function deleteConfirmed(mixed $id)
+    {
+        parent::deleteConfirmed(ModelsUser::findOrFail($id));
+        session()->flash('status', 'Deleted successfully');
+    }
+
+    public function resetValues()
+    {
+        parent::resetValues();
+        $this->login =
+            $this->role_id = null;
+    }
+
+    public function resetPassword(ModelsUser $item)
+    {
+        $item->password = Hash::make(self::DEFAULT_PASSWORD);
+        $item->save();
+        $this->resetValues();
+        session()->flash('status', 'Changed successfully');
+    }
+
+    #[Layout('livewire.layouts.base')]
+    #[Title('Boutique | User')]
     public function render()
     {
         return view('livewire.user', [
             'roles' => Role::all(),
             'users' => ModelsUser::all(),
-        ])->layout('livewire.layouts.base');
+        ]);
     }
 }
--- resources/views/livewire/user.blade.php
+++ resources/views/livewire/user.blade.php
@@ -6,18 +6,25 @@
                 <input wire:model.live="login" id="login" type="text">
                 @error('login') <span>{{ $message }}</span> @enderror
             </div>
+            @empty($this->edit_id)
+                <div>
+                    <label for="role">Role</label>
+                    <select wire:model="role_id" name="role_id" id="role">
+                        <option>Choisir un rôle</option>
+                        @foreach ($roles as $item)
+                            <option wire:key="{{ $item->id }}" value="{{ $item->id }}">{{ $item->libelle }}</option>
+                        @endforeach
+                    </select>
+                    @error('role') <span>{{ $message }}</span> @enderror
+                </div>
+            @endempty
+
             <div>
-                <label for="role">Role</label>
-                <select wire:model="role" name="role" id="role">
-                    @foreach ($roles as $item)
-                        <option wire:key="{{ $item->id }}" value="{{ $item->id }}">{{ $item->libelle }}</option>
-                    @endforeach
-                </select>
-                @error('role') <span>{{ $message }}</span> @enderror
-            </div>
-            <div>
-                <input type="submit" value="{{ 'Valider' }}">
-                <button>Réinitialiser le mot de passe</button>
+                <input type="submit" value="{{ $this->textSubmit }}">
+                <input wire:click='resetValues' type="reset" value="Annuler">
+                @if ($this->edit_id)
+                    <button wire:click='resetPassword({{ $this->edit_id }})'>Réinitialiser le mot de passe</button>
+                @endif
             </div>
         </form>
     </div>
@@ -31,11 +38,41 @@
         </thead>
         <tbody>
             @foreach ($users as $item)
-                <tr>
+                <tr
+                    wire:click='editItem({{ $item->id }})'
+                    wire:dblclick='deleteItem({{ $item->id }})'
+                >
                     <td>{{ $item->login }}</td>
-                    <td>{{ $item->role }}</td>
+                    <td>{{ $item->role->libelle }}</td>
                 </tr>
             @endforeach
         </tbody>
     </table>
+
+    <!-- deleteModal -->
+    <div wire:ignore.self class="modal fade" id="confirmModal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="viewModalLabel">Confirmer la suppression</h5>
+                    <button class="btn-close" wire:click="deleteCancelled()" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Suppression de &lt; {{ $this->login }} &gt;</p>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary btn-sm" wire:click="deleteConfirmed({{ $this->delete_id }})">
+                        Valider
+                    </button>
+                    <button class="btn btn-primary btn-sm" wire:click="deleteCancelled" data-bs-dismiss="modal">
+                        Annuler
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    @if (session()->has('status'))
+        <div class="alert alert-success text-center">{{ session('status') }}</div>
+    @endif
 </div>
