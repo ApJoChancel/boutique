466c01213eb8cf4df8ab9c83e6a09fb669be6648
MBA NDONG Chancel <mbandongjohan@gmail.com>
2023-09-15 11:08:30 +0100

    Styles : boutique

--- resources/views/livewire/boutique.blade.php
+++ resources/views/livewire/boutique.blade.php
@@ -1,53 +1,83 @@
 <div>
-    <div>
-        <form wire:submit="save">
-            <div>
-                <label for="designation">Désignation</label>
-                <input wire:model.live="designation" id="designation" type="text">
-                @error('designation') <span>{{ $message }}</span> @enderror
-            </div>
-            @if(!$this->edit_id)
-                <div>
-                    <label for="user">Manager</label>
-                    <select wire:model="user_id" name="user_id" id="user">
-                        <option>Choisir un manager</option>
-                        @foreach ($users as $item)
-                            <option wire:key="{{ $item->id }}" value="{{ $item->id }}">{{ $item->login }}</option>
-                        @endforeach
-                    </select>
-                    @error('user') <span>{{ $message }}</span> @enderror
+    <form wire:submit="save">
+        <div class="mt-8 bg-white p-4 shadow rounded-lg">
+            <div class="-mx-3 md:flex mb-2">
+                <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label for="designation" class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                        Désignation
+                    </label>
+                    <input wire:model.live="designation" id="designation" type="text" placeholder="designation" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4">
+                    @error('designation') <p class="text-grey-dark text-xs italic">{{ $message }}</p> @enderror
                 </div>
-            @endif
-
-            <div>
-                <input type="submit" value="{{ $this->textSubmit }}">
-                <input wire:click='resetValues' type="reset" value="Annuler">
+                @if(!$this->edit_id)
+                    <div class="md:w-1/2 px-3">
+                        <label for="user" class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2">
+                            Manager
+                        </label>
+                        <div class="relative">
+                            <select wire:model="user_id" name="user_id" id="user" class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded">
+                                <option>Choisir un manager...</option>
+                                @foreach ($users as $item)
+                                    <option wire:key="{{ $item->id }}" value="{{ $item->id }}">{{ $item->noms }}</option>
+                                @endforeach
+                            </select>
+                            <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
+                                <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                            </div>
+                        </div>
+                    </div>
+                @endif
             </div>
-        </form>
-        @if ($this->edit_id)
-            <button wire:click='changeManager({{ $this->edit_id }})'>Changer le manager</button>
-        @endif
-    </div>
+            <div>
+                <button type="submit" class="py-2 px-4 bg-transparent text-green-600 font-semibold border border-green-600 rounded hover:bg-green-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
+                    {{ $this->textSubmit }}
+                </button>
+                <button  wire:click='resetValues' type="reset" class="py-2 px-4 bg-transparent text-purple-600 font-semibold border border-purple-600 rounded hover:bg-purple-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
+                    Annuler
+                </button>
+                @if ($this->edit_id)
+                    <button type="button" wire:click='changeManager({{ $this->edit_id }})' class="py-2 px-4 bg-transparent text-blue-600 font-semibold border border-blue-600 rounded hover:bg-blue-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
+                        Changer le manager
+                    </button>
+                    <button type="button" wire:click='deleteItem({{ $this->edit_id }})' class="py-2 px-4 bg-transparent text-red-600 font-semibold border border-red-600 rounded hover:bg-red-600 hover:text-white hover:border-transparent transition ease-in duration-200 transform hover:-translate-y-1 active:translate-y-0">
+                        Supprimer
+                    </button>
+                @endif
+            </div>
+        </div>
+    </form>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Boutique</th>
-                <th>Manager</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($boutiques as $item)
-                <tr
-                    wire:click='editItem({{ $item->id }})'
-                    wire:dblclick='deleteItem({{ $item->id }})'
-                >
-                    <td>{{ $item->designation }}</td>
-                    <td>{{ $item->manager?->login }}</td>
+    <div class="mt-8 bg-white p-4 shadow rounded-lg">
+        <h2 class="text-gray-500 text-lg font-semibold pb-4">Les boutiques</h2>
+        <div class="my-1"></div> <!-- Espacio de separación -->
+        <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mb-6"></div> <!-- Línea con gradiente -->
+        <table class="w-full table-auto text-sm">
+            <thead>
+                <tr class="text-sm leading-normal">
+                    <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">
+                        Boutique
+                    </th>
+                    <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light">
+                        Manager
+                    </th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($boutiques as $item)
+                    <tr class="hover:bg-grey-lighter"
+                        wire:click='editItem({{ $item->id }})'
+                    >
+                        <td class="py-2 px-4 border-b border-grey-light">
+                            {{ $item->designation }}
+                        </td>
+                        <td class="py-2 px-4 border-b border-grey-light">
+                            {{ $item->manager?->noms }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
     <!-- deleteModal -->
     <div wire:ignore.self class="modal fade" id="confirmModal">
@@ -90,7 +120,7 @@
                             <select wire:model="user_id" name="user_id" id="user">
                                 <option>Choisir un manager</option>
                                 @foreach ($users as $item)
-                                    <option wire:key="{{ $item->id }}" value="{{ $item->id }}">{{ $item->login }}</option>
+                                    <option wire:key="{{ $item->id }}" value="{{ $item->id }}">{{ $item->noms }}</option>
                                 @endforeach
                             </select>
                         </div>
--- resources/views/livewire/layouts/base.blade.php
+++ resources/views/livewire/layouts/base.blade.php
@@ -44,35 +44,45 @@
             <!-- Barra lateral de navegación (oculta en dispositivos pequeños) -->
             <div class="p-2 bg-white w-full md:w-60 flex flex-col md:flex hidden" id="sideNav">
                 <nav>
-                    <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white" href="#">
-                        <i class="fas fa-home mr-2"></i>Inicio
+                    <a href="#" class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white">
+                        <i class="fas fa-home mr-2"></i>Tableau de bord
                     </a>
-                    <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white" href="#">
-                        <i class="fas fa-file-alt mr-2"></i>Autorizaciones
-                    </a>
-                    <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white" href="#">
-                        <i class="fas fa-users mr-2"></i>Usuarios
-                    </a>
-                    <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white" href="#">
-                        <i class="fas fa-store mr-2"></i>Comercios
-                    </a>
-                    <a class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white" href="#">
-                        <i class="fas fa-exchange-alt mr-2"></i>Transacciones
-                    </a>
+
+                    <div class="relative">
+                        <a href="#" id="groupBoutique" class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white">
+                            <i class="fas fa-file-alt mr-2"></i>Boutiques & utilisateurs
+                        </a>
+
+                        <div id="groupBoutiqueContent" class="absolute left-14 top-0 hidden mt-2 space-y-2 bg-white border border-gray-200 w-40">
+                            <a href="{{ route('user') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Utilisateurs</a>
+                            <a href="{{ route('boutique') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Boutiques</a>
+                        </div>
+                    </div>
+                    <div class="relative">
+                        <a href="#" id="groupArticle" class="block text-gray-500 py-2.5 px-4 my-4 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white">
+                            <i class="fas fa-file-alt mr-2"></i>Articles
+                        </a>
+
+                        <div id="groupArticleContent" class="absolute left-14 top-0 hidden mt-2 space-y-2 bg-white border border-gray-200 w-40">
+                            <a href="{{ route('categorie') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Catégories</a>
+                            <a href="{{ route('carac') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Caractéristiques</a>
+                            <a href="{{ route('article') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Articles</a>
+                        </div>
+                    </div>
                 </nav>
-    
+
                 <!-- Ítem de Cerrar Sesión -->
-                <a class="block text-gray-500 py-2.5 px-4 my-2 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white mt-auto" href="#">
-                    <i class="fas fa-sign-out-alt mr-2"></i>Cerrar sesión
+                <a href="{{ route('logout') }}" class="text-gray-500 py-2.5 px-4 my-2 rounded transition duration-200 hover:bg-gradient-to-r hover:from-cyan-500 hover:to-cyan-500 hover:text-white mt-auto">
+                    <i class="fas fa-sign-out-alt mr-2"></i>Déconnexion
                 </a>
-    
+
                 <!-- Señalador de ubicación -->
                 <div class="bg-gradient-to-r from-cyan-300 to-cyan-500 h-px mt-2"></div>
-    
+
                 <!-- Copyright al final de la navegación lateral -->
                 <p class="mb-1 px-5 py-3 text-left text-xs text-cyan-500">Copyright WCSLAT@2023</p>
             </div>
-    
+
             <!-- Área de contenido principal -->
             <div class="flex-1 p-4 w-full md:w-1/2">
                 <!-- Campo de búsqueda -->
@@ -106,6 +116,14 @@
         });
         window.addEventListener('show-change', event => {
             $('#changeModal').modal('show')
+        });
+    </script>
+    <script>
+        document.getElementById('groupBoutique').addEventListener('click', function() {
+            document.getElementById('groupBoutiqueContent').classList.toggle('hidden');
+        });
+        document.getElementById('groupArticle').addEventListener('click', function() {
+            document.getElementById('groupArticleContent').classList.toggle('hidden');
         });
     </script>
     
