44e59c674a1545bd99aac793a05b119c3c92a4c6
MBA NDONG Chancel <mbandongjohan@gmail.com>
2023-11-01 16:04:27 +0100

    Fonction en fonction du USER : Stat Non Conclue

--- app/Livewire/StatNonConclue.php
+++ app/Livewire/StatNonConclue.php
@@ -9,6 +9,12 @@ use Livewire\Attributes\Title;
 
 class StatNonConclue extends AppComponent
 {
+    public function mount()
+    {
+        //Boutiques valides
+        $this->boutiques_valides = $this->boutiqueValide();
+    }
+
     #[Layout('livewire.layouts.base')]
     #[Title('Boutique | Statistiques - Visites non conclues')]
     public function render()
@@ -17,6 +23,7 @@ class StatNonConclue extends AppComponent
             ->selectRaw("
                 COUNT(visites.id) AS nombre
             ")
+            ->whereIn('visites.boutique_id', $this->boutiques_valides)
             ->first()
         ;
 
@@ -25,6 +32,7 @@ class StatNonConclue extends AppComponent
                 COUNT(visites.id) AS nombre
             ")
             ->where('visites.conclue', true)
+            ->whereIn('visites.boutique_id', $this->boutiques_valides)
             ->first()
         ;
         // dd($total);
@@ -39,6 +47,7 @@ class StatNonConclue extends AppComponent
             ->join('choix', 'reponses.choix_id', 'choix.id')
             ->join('questions', 'choix.question_id', 'questions.id')
             ->where('visites.conclue', false)
+            ->whereIn('visites.boutique_id', $this->boutiques_valides)
             // ->join('users', 'visites.user_id', 'users.id')
             // ->join('boutiques', 'users.boutique_id', 'boutiques.id')
             ->groupBy('questions.id', 'choix.id', 'questions.libelle', 'choix.libelle')
